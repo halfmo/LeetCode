454f8fd235d7cdcac492813d0c61ed470b613d6a
halfmo <1391643993@qq.com>
2022-03-12 16:58:05 +0800

    第八天

--- src/Easy/206.反转链表/Reverselist.java
+++ src/Easy/206.反转链表/Reverselist.java
@@ -13,6 +13,7 @@ public class Reverselist {
         ListNode cur=head;
         ListNode pre=null;
         ListNode temp=null;
+        //cur负责和原来的一样往后推进，然后使用temp来作为交换的临时变量，在用pre来记录之前的点再将cur的next指向pre就可以实现链表的原地反转
         while(cur!=null){//当当前的不为空
             temp=cur.next;//用临时变量来保存原来的方向
             cur.next=pre;//在此处进行反转，将链表的方向反转
---
+++ src/数据结构基础/KMP算法（简单）/KMP.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class KMP {
+    public static void main(String[] args){
+        char[] str = "abababaaababaa".toCharArray();
+        char[] sub = "ababaaababaa".toCharArray();
+
+        int[] next = new int[sub.length];
+
+        int i = 1,j;
+        while (i < next.length){
+            j = i - 1;
+            while (true){
+                if (next[j] == 0 || sub[i - 1] == sub[next[j] - 1]){
+                    next[i] = next[j] + 1;
+                    break;
+                }
+                j = next[j] - 1;
+            }
+            i++;
+        }
+        System.out.println("序列为："+ Arrays.toString(sub));
+        System.out.println("子序列为："+Arrays.toString(next));
+        i = 0;
+        j = 0;
+        while(i < str.length){
+            if(str[i] == sub[j]){
+                i++;
+                j++;
+            }else {
+                if (j == 0) i++;
+                else j = next[j] - 1;
+            }
+            if (j == sub.length){
+                System.out.println("匹配成功");
+                break;
+            }
+        }
+    }
+}

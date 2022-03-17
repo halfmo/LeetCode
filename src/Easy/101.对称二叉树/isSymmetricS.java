9f8c9072aec7750e604cf5f0fc8b77413d8af738
halfmo <1391643993@qq.com>
2022-03-17 19:22:11 +0800

    第十天

---
+++ src/Easy/101.对称二叉树/isSymmetricS.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class isSymmetricS {
+      class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return compare(root.left,root.right);
+    }
+    public boolean compare(TreeNode left, TreeNode right){
+          if (left == null && right != null)return false;
+          else if(left != null && right == null) return false;
+          else if(left == null && right == null) return true;
+          else if (left.val != right.val)return false;
+          boolean outside = compare(left.left,right.right);
+          boolean inside = compare(left.right,right.left);
+          boolean isSame = outside && inside;
+          return isSame;
+    }
+}
